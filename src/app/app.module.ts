e56f4cd707ac05c77ac31b48e8d1db3390a5795d
Kondzik998 <154377@student.uwm.edu.pl>
2020-07-24 01:42:01 +0200

    InitialCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { NavComponent } from './navbar/nav/nav.component';
+import { PropertyListComponent } from './property/property-list/property-list/property-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyCardComponent,
+    NavComponent,
+    PropertyListComponent
+
   ],
   imports: [
     BrowserModule
