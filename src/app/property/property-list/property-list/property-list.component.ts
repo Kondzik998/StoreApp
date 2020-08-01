3aea2f5992e77625f88e08b8b2be6ed841d4df0d
Kondzik998 <154377@student.uwm.edu.pl>
2020-08-01 23:25:47 +0200

    fixed commit

--- src/app/property/property-list/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list/property-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { CarsiService } from 'src/app/services/carsi.service';
 
 @Component({
